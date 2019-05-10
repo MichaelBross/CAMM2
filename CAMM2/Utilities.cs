b4897c44465a67bd20983680327ac75b3336e171
Michael Bross <michaelbross31@gmail.com>
2019-05-09 21:03:34 -0400

    Cleaned.

--- CAMM2/Utilities.cs
+++ CAMM2/Utilities.cs
@@ -64,64 +64,97 @@ namespace Presentation
     {
         public static string JsonKoObject(object viewModel)
         {
-            var jsonKoObject = "{";
+            var jsonKoObject = "vm.Item = {\r\n";
             foreach (var p in viewModel.GetType().GetProperties())
             {
                 if (p.PropertyType == typeof(string))
                 {
-                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),";
+                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType == typeof(DateTime))
                 {
-                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(new Date()),";
+                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(new Date()),\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType == typeof(int))
                 {
-                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),";
+                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType.BaseType == typeof(Enum))
                 {
-                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),";
+                    jsonKoObject = jsonKoObject + p.Name + ": ko.observable(),\r\n";
                 }
 
             }
-            jsonKoObject = jsonKoObject + "}";
+            jsonKoObject = jsonKoObject + "}\r\n";
 
             return jsonKoObject;
         }
 
         public static string ClearModelFunction(object viewModel)
         {
-            var jsonKoObject = "{";
+            var clearModelFunc = "vm.clearModel = function () {\r\n";
             foreach (var p in viewModel.GetType().GetProperties())
             {
                 if (p.PropertyType == typeof(string))
                 {
-                    jsonKoObject = jsonKoObject + "vm.Item." + p.Name + "('');";
+                    clearModelFunc = clearModelFunc + "vm.Item." + p.Name + "('');\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType == typeof(DateTime))
                 {
-                    jsonKoObject = jsonKoObject + "vm.Item." + p.Name + "(vm.today());";
+                    clearModelFunc = clearModelFunc + "vm.Item." + p.Name + "(vm.today());\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType == typeof(int))
                 {
-                    jsonKoObject = jsonKoObject + "vm.Item." + p.Name + "('');";
+                    clearModelFunc = clearModelFunc + "vm.Item." + p.Name + "('');\r\n";
+                    continue;
                 }
 
                 if (p.PropertyType.BaseType == typeof(Enum))
                 {
-                    jsonKoObject = jsonKoObject + "vm.Item." + p.Name + "('');";
+                    clearModelFunc = clearModelFunc + "vm.Item." + p.Name + "('');\r\n";
                 }
 
             }
-            jsonKoObject = jsonKoObject + "}";
+            clearModelFunc = clearModelFunc + "}";
 
-            return jsonKoObject;
+            return clearModelFunc;
+        }
+
+        public static string LoadDetailsFunction(object viewModel)
+        {
+            var loadDetailsFunc = "vm.loadDetails = function (data) {\r\n";
+            loadDetailsFunc += "if (data === undefined){\r\n";
+            loadDetailsFunc += "    vm.clearModel();\r\n";
+            loadDetailsFunc += "    return;\r\n";
+            loadDetailsFunc += "}\r\n";
+
+            foreach (var p in viewModel.GetType().GetProperties())
+            {
+                if (p.PropertyType == typeof(DateTime))
+                {
+                    loadDetailsFunc += "vm.Item." + p.Name + "(vm.formatDate(data['" + p.Name + "']));\r\n";
+                    continue;
+                }
+                else
+                {
+                    loadDetailsFunc += "vm.Item." + p.Name + "(data['" + p.Name + "']);\r\n";
+                }
+
+
+            }
+            loadDetailsFunc = loadDetailsFunc + "}";
+
+            return loadDetailsFunc;
         }
 
         public static MvcHtmlString TableColumns(object viewModel)
@@ -136,16 +169,6 @@ namespace Presentation
                     visible = hiddenInput.DisplayValue;
                 }
 
-                if (p.Name == "Id")
-                {
-                    columnsList.Add(new JsColumn(p.Name, p.Name, p.Name, visible));
-                }
-
-                if (p.PropertyType == typeof(string))
-                {
-                    columnsList.Add(new JsColumn(p.Name, p.Name, p.Name, visible));
-                }
-
                 if (p.PropertyType == typeof(DateTime))
                 {
                     var render = "function(data, type, row, meta) { " +
@@ -153,13 +176,9 @@ namespace Presentation
                         "}";
 
                     columnsList.Add(new JsColumn(render, p.Name, p.Name, visible));
+                    continue;
                 }
-
-                if (p.PropertyType == typeof(int) && p.Name != "Id")
-                {
-                    columnsList.Add(new JsColumn(p.Name, p.Name, p.Name, visible));
-                }
-
+                
                 if (p.PropertyType.BaseType == typeof(Enum))
                 {                    
                     var render = "function(data, type, row, meta) { " +
@@ -167,8 +186,10 @@ namespace Presentation
                         "}";
 
                     columnsList.Add(new JsColumn(render, p.Name, p.Name, visible));
+                    continue;
                 }
 
+                columnsList.Add(new JsColumn(p.Name, p.Name, p.Name, visible));
             }            
             
             var columnsMvcHtmlString = new MvcHtmlString(JsonConvert.SerializeObject(columnsList));
@@ -179,10 +200,30 @@ namespace Presentation
             unwantedString = "]);}\"";
             withThis = "]);}";
             var finished = corrected.Replace(unwantedString, withThis);
+            //unwantedString = "},";
+            //withThis = "},\r\n";
+            //finished = corrected.Replace(unwantedString, withThis);
             var backToMvcHtmlString = new MvcHtmlString(finished);
 
 
             return backToMvcHtmlString;
+        }
+
+        public static string SelectLists(object detailVM)
+        {
+            var selectLists = "";
+            foreach (var p in detailVM.GetType().GetProperties())
+            {
+                if (p.PropertyType.BaseType == typeof(Enum))
+                {
+                    var SelectListName = p.Name + "List";
+                        
+                        selectLists += "vm." + SelectListName + " = ko.observableArray(" + SelectListFromEnum(p.PropertyType) + ")";
+                        
+                    }
+
+            }
+            return selectLists;
         }
 
         public static MvcHtmlString SelectListFromEnum(Type type)
