4b875caf75f4152e17d35de541348c924add45f5
Michael Bross <michaelbross31@gmail.com>
2019-07-31 19:39:10 -0400

    Implemented building of Doman Classes with a T4 template. Excluded original hand coded classes. Updated CAMM2 XML model and affected templates accordingly.

---
+++ CAMM2.Repository/ClassesGenerated.cs
@@ -0,0 +1,127 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+	public class User
+	{	 
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string EmailAddress { get; set; }
+
+		public bool IsObsolete { get; set; }
+
+		public DateTime CreateDate { get; set; }
+
+		public int CreatedBy { get; set; }
+
+		public DateTime UpdateDate { get; set; }
+
+		public int UpdatedBy { get; set; }
+
+	}
+	public class Document : Base
+	{	 
+		public int Id { get; set; }
+
+		public string Code { get; set; }
+
+		public string Rev { get; set; }
+
+		public string Title { get; set; }
+
+		public DocumentType DocType { get; set; }
+
+		public IList<Item> Items { get; set; }
+
+	}
+	public class Component : Item
+	{	 
+		public string Manufacturer { get; set; }
+
+	}
+	public class Connector : Item
+	{	 
+		public string Family { get; set; }
+
+		public string Comments { get; set; }
+
+		public IList<Tool> Tools { get; set; }
+
+	}
+	public class Contact : Item
+	{	 
+		public string Size { get; set; }
+
+		public string Family { get; set; }
+
+		public int WireGageMin { get; set; }
+
+		public int WireGageMax { get; set; }
+
+		public string Comments { get; set; }
+
+		public IList<Tool> Tools { get; set; }
+
+	}
+	public class Item : Base
+	{	 
+		public int Id { get; set; }
+
+		[Index(IsUnique = true)]
+		[StringLength(100)]
+		public string Code { get; set; }
+
+		public string Description { get; set; }
+
+		public UnitsOfMeasure UnitsOfMeasure { get; set; }
+
+		public int QtyOnHand { get; set; }
+
+		public IList<Document> Documents { get; set; }
+
+	}
+	public class Tool : Item
+	{	 
+		public string Manufacturer { get; set; }
+
+		public string BinNumber { get; set; }
+
+		public string MilitarySpecification { get; set; }
+
+		public string SerialNumber { get; set; }
+
+		public string Comments { get; set; }
+
+		public IList<Connector> Connectors { get; set; }
+
+		public IList<Contact> Contacts { get; set; }
+
+	}
+	public class Assembly : Item
+	{	 
+		public string Rev { get; set; }
+
+		public IList<AssemblyComponent> AssemblyComponents { get; set; }
+
+	}
+	public class AssemblyComponent : Base
+	{	 
+		public int Id { get; set; }
+
+		public Assembly Assembly { get; set; }
+
+		public Component Component { get; set; }
+
+		public Decimal Qty { get; set; }
+
+	}
+}
+
---
+++ CAMM2.Repository/Enums/DocumentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum DocumentType
+    {
+        Drawing = 1,
+        PartsList = 2,
+        DataSheet = 3,
+        MSDS = 4,
+        Catalog = 5
+    }
+}
